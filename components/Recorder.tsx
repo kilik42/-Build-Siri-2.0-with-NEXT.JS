49cbd70857207b5b08a15d40413d8f9298e2b948
kilik42 <marvinevins@yahoo.com>
2024-03-17 23:05:55 -0500

    first commit

---
+++ components/Recorder.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Recorder() {
+  return (
+    <div className='flex items-center justify-center  text-white'>
+      recorder
+    </div>
+  )
+}
+
+export default Recorder
---
+++ components/messages.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function messages() {
+  return (
+    <div className='flex flex-col min-h-screen  p-5 pt-20'>
+      messages
+    </div>
+  )
+}
+
+export default messages
